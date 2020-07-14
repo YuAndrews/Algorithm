970a2e53f9719b6fe19a0324141bc2e1559bdbc7
andrew <646972144@qq.com>
2020-07-14 15:28:33 +0800

    移除重复元素

---
+++ leetcode/delrepeat.php
@@ -0,0 +1,31 @@
+<?php
+
+$arr = [1,1,1,2,2,3,4,4,5,5,6,6,6,6,7,8,9,9];
+
+
+function delRepeat(&$arr){
+    $len = count($arr);
+    $count = 1;
+    $num = [];
+    for ($i=1;$i<$len;$i++){
+        if($arr[$i] == $arr[$i-1]){
+            $count++;
+            if($count>2){
+//                unset($arr[$i]);
+//                var_dump($arr);die;
+                $num[] = $i;
+                $count--;
+            }
+        }else{
+            $count = 1;
+        }
+    }
+    if($num){
+        foreach ($num as $j){
+            unset($arr[$j]);
+        }
+    }
+}
+
+delRepeat($arr);
+var_dump($arr);
